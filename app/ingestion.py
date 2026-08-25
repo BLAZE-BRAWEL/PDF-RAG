65d094632a43d5ed51531c21fe8a30305d681bad
BLAZE-BRAWEL <blazebrawel@gmail.com>
2026-08-26 04:27:13 +0600

    Switched to PyMuPDF4LLM from PyMyPDF with MarkDownHeaderTextSplitter for better quality of Ingestion

--- app/ingestion.py
+++ app/ingestion.py
@@ -1,33 +1,17 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter, MarkdownHeaderTextSplitter
 from sentence_transformers import SentenceTransformer
-import pymupdf
+import pymupdf4llm
 from langchain_core.documents import Document
 
 model = SentenceTransformer("BAAI/bge-base-en-v1.5")
 
 def load_pdf(path):
-    doc = pymupdf.open(path)
+    doc = pymupdf4llm.to_markdown(path)
 
-    pages = []
-
-    for page in doc:
-        text = page.get_text("text", sort=True)
-
-
-        pages.append(text)
+    pdf_text = doc
     
-    doc.close()
-
-    pdf_text = " ".join(page for page in pages)
     
-    document = Document(
-        page_content = pdf_text,
-        metadata = {
-            "source" : str(path)
-        }
-    )
-    
-    return document
+    return pdf_text
 
 class Ingestion():
     
@@ -39,10 +23,19 @@ class Ingestion():
     
     def chunk_documents_with_embedding(self, document, chunk_size, chunk_overlap):
         
-        splitter = RecursiveCharacterTextSplitter(chunk_size= chunk_size, chunk_overlap = chunk_overlap)
+        
+        splitter_headers = [
+            ("#", "Header 1"),
+            ("##", "Header 2"),
+            ("###", "Header 3")
+        ]
+        
+        splitter = MarkdownHeaderTextSplitter(
+            headers_to_split_on= splitter_headers
+        )
         
         
-        chunks = splitter.split_documents([document])
+        chunks = splitter.split_text(document)
         
         
         texts = [
