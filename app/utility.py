71fb3fc141d864d59a7aca73f03dbdf8cb576293
BLAZE-BRAWEL <blazebrawel@gmail.com>
2026-08-29 22:28:08 +0600

    Changd Utility function so it can create fingerPrint for PDF to check for duplication

--- app/utility.py
+++ app/utility.py
@@ -1,5 +1,11 @@
 from pydantic import SecretStr
 from pwdlib import PasswordHash
+import hashlib
+from .dependency import get_qdrant
+from qdrant_client import QdrantClient
+from qdrant_client.models import MatchValue, FieldCondition, Filter
+from fastapi import Depends
+
 
 hashing_algorithm = PasswordHash.recommended()
 
@@ -17,3 +23,30 @@ def verify_password(password: str, hashed_password: str):
         hashed_password
     )
 
+def finger_print_for_pdf(file: bytes) -> str:
+    
+    return hashlib.sha256(file).hexdigest()
+
+def verify_pdf_finger_print(collection_name: str,file: bytes ,qdrant: QdrantClient= Depends(get_qdrant)):
+    
+    finger_print = finger_print_for_pdf(file)
+    
+    duplicate_check = qdrant.scroll(
+        collection_name= collection_name,
+        scroll_filter= Filter(
+            must = [
+                FieldCondition(
+                    key = "fignerprint",
+                    match = MatchValue(value = finger_print)
+                )
+            ]
+        ),
+        
+        limit = 1,
+        with_payload = False,
+        with_vectors = False
+    )
+    
+    point , next_starting_point = duplicate_check
+    
+    return point
